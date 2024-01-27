1374625e9df85466187d85c97e7601ef73e95f7a
eunung <eunung24@gmail.com>
2024-01-27 12:02:40 +0900

    feat(front): 불필요한 파일 삭제

--- apps/client/src/utils/getUserData.ts
+++
@@ -1,8 +0,0 @@
-// import { doc, getDoc } from 'firebase/firestore';
-// import { db } from './firebase';
-
-// export const getUserData = async (collection: string, docs: string) => {
-//   const docRef = doc(db, collection, docs);
-//   const docSnap = await getDoc(docRef);
-//   return docSnap.data();
-// };

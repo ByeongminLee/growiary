6c93b043fe2a24febb4639395b2108edb09d728e
eunung <eunung24@gmail.com>
2024-01-28 16:26:30 +0900

    feat(front): 기본 metatag 설정

---
+++ apps/client/src/utils/appInfo.ts
@@ -0,0 +1,7 @@
+export const APP_INFO = {
+  NAME: 'Growiary',
+  DEFAULT_TITLE: 'Growiary',
+  TITLE_TEMPLATE: '%s - Growiary',
+  DESCRIPTION: 'Growth Diary with AI',
+  URL: new URL('http://localhost:3000'),
+};

9c3a92c31295da1bea7a6f3ff5101ad8c322c407
eunung <eunung24@gmail.com>
2024-02-02 17:30:28 +0900

    update(front): 소셜 로그인 (카카오, 네이버) 연결

--- apps/client/src/components/home/LoginView.tsx
+++ apps/client/src/components/home/LoginView.tsx
@@ -13,7 +13,7 @@ export const LoginView = () => {
           SNS 계정으로 간편하게 가입하세요
         </p>
         <div className="flex gap-x-[20px]">
-          <SocialLoginBtn type="google">
+          <SocialLoginBtn type="naver">
             <Image
               width={54}
               height={54}
@@ -29,7 +29,7 @@ export const LoginView = () => {
               alt="google-login"
             />
           </SocialLoginBtn>
-          <SocialLoginBtn type="google">
+          <SocialLoginBtn type="kakao">
             <Image
               width={54}
               height={54}

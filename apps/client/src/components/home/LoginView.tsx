2585c30e81ff51d2c145161ac1bd38e453e68094
eunung <eunung24@gmail.com>
2024-02-02 22:16:16 +0900

    feat(front): 로그인 이미지 적용

--- apps/client/src/components/home/LoginView.tsx
+++ apps/client/src/components/home/LoginView.tsx
@@ -4,9 +4,23 @@ import Image from 'next/image';
 export const LoginView = () => {
   return (
     <main className="layout-full">
-      <section className="self-start">
-        <div className="bg-temp-img w-[312px] h-[113px] mb-[23px]">Logo</div>
-        <h1 className="font-p-M24 text-primary-600">AI와 함께하는 회고 다이어리</h1>
+      <section className="self-start mt-18">
+        <Image src="/assets/app/logo.png" alt="growiary" width={312} height={113} />
+        <h1 className="font-p-M24 text-primary-900 mt-2">AI와 함께하는 회고 다이어리</h1>
+        <Image
+          src="/assets/app/line.svg"
+          alt="line"
+          width={587}
+          height={5}
+          className="absolute top-[416px] inset-x-0 w-screen"
+        />
+        <Image
+          src="/assets/app/growmi_with_person.png"
+          alt="grwomi_with_person"
+          width={128}
+          height={128}
+          className="absolute top-[336px] right-[20px]"
+        />
       </section>
       <section className="flex flex-col items-center">
         <p className="font-p-M14 text-grayscale-600 mb-[19px]">

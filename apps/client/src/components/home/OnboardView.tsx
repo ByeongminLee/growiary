8f44b6b74c1cd98771916a68d3c528f2e2d17267
eunung <eunung24@gmail.com>
2024-02-03 11:59:32 +0900

    update(front): 온보딩 이미지 변경

--- apps/client/src/components/home/OnboardView.tsx
+++ apps/client/src/components/home/OnboardView.tsx
@@ -4,8 +4,9 @@ import Carousel from '@/components/ui/carousel/Carousel';
 import { useState } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/shadcn/button';
+import Image from 'next/image';
 
-const TempSlide = ({ text }: { text: string }) => {
+const Slide = ({ text }: { text: string }) => {
   return <div className="w-full h-[440px] bg-temp-img">{text}</div>;
 };
 
@@ -21,8 +22,17 @@ const OnboardView = () => {
     <section className="layout-full h-[calc(85vh-72px)]">
       <div className="grow flex items-center">
         <Carousel onReachEnd={onReachEnd} width="312px">
-          {slideContents.map((content, idx) => (
-            <TempSlide key={idx} text={content} />
+          {[...new Array(4)].map((v, i) => (
+            <Image
+              style={{
+                margin: '0 auto',
+              }}
+              key={i + 1}
+              src={`/assets/images/onboarding${i + 1}.png`}
+              alt={`onboarding${i + 1}`}
+              width={312}
+              height={440}
+            />
           ))}
         </Carousel>
       </div>

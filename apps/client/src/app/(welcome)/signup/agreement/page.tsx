76aaa6c6ae741fc0569c32a574c454234abce396
eunung <eunung24@gmail.com>
2024-02-05 15:25:59 +0900

    update(front): 미로그인 시 닉네임 설정, 서비스 이동 동의 화면 -> 로그인 화면 리다이렉트

--- apps/client/src/app/(welcome)/signup/agreement/page.tsx
+++ apps/client/src/app/(welcome)/signup/agreement/page.tsx
@@ -1,5 +1,14 @@
 import ServiceTermView from '@/components/home/ServiceTermView';
+import NickNameSettingView from '@/components/home/NickNameSettingView';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/utils/authOptions';
 
 export default async function AgreementPage() {
-  return <ServiceTermView />;
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    return <ServiceTermView />;
+  }
+  redirect('/login');
 }

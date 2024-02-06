9500f713c02445278f5afce24729b68611159010
eunung <eunung24@gmail.com>
2024-02-06 09:54:24 +0900

    update(front): APP_INFO 수정

--- apps/client/src/utils/appInfo.ts
+++ apps/client/src/utils/appInfo.ts
@@ -1,7 +1,7 @@
 export const APP_INFO = {
-  NAME: 'Growiary',
-  DEFAULT_TITLE: 'Growiary',
-  TITLE_TEMPLATE: '%s - Growiary',
+  NAME: '그루어리',
+  DEFAULT_TITLE: '그루어리',
+  TITLE_TEMPLATE: '%s - 그루어리',
   DESCRIPTION: 'Growth Diary with AI',
   URL: new URL('https://growiary.com'),
 };

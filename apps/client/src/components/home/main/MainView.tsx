193eafe5a73bf97b14e4588d26efd31704b50c34
eunung <eunung24@gmail.com>
2024-02-13 17:04:18 +0900

    feat(front): 오타 수정

--- apps/client/src/components/home/main/MainView.tsx
+++ apps/client/src/components/home/main/MainView.tsx
@@ -239,7 +239,7 @@ const MainView = () => {
           <AlertDialogContent className="flex justify-center p-0 m-0 rounded-md bg-tranparent border-0">
             <div className="px-3 py-2 bg-grayscale-300 flex flex-col items-center jusitfy-center rounded">
               <p className="pb-2 font-p-M14 text-grayscale-700">
-                구르미가 답장을 쓰고 있어요
+                그루미가 답장을 쓰고 있어요
               </p>
               <LottieAnimation src={airplane} className="bg-primary-100" />
             </div>
